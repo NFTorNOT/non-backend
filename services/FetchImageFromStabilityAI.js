ff76f5a8fc71456459e17e0a84ba9a17ab1596e8
vindeep07 <vindeepchaudhari@gmail.com>
2022-12-03 11:35:15 +0530

    Updated aws env vars

--- services/FetchImageFromStabilityAI.js
+++ services/FetchImageFromStabilityAI.js
@@ -374,8 +374,8 @@ class FetchImageFromStabilityAI {
 
       const AWSInstance = new AWS.S3({
         region: 'us-east-1',
-        accessKeyId: process.env.S3_ACCESS_KEY_ID,
-        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
       });
 
       return AWSInstance;
