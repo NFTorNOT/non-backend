b0e41d83ad0653cec572000a0b35a518abaa9e7e
VRDighe <vaibhav@plgworks.com>
2022-12-06 13:08:57 +0530

    added temp contract

--- helpers/nftOrNotContract.js
+++ helpers/nftOrNotContract.js
@@ -1,7 +1,8 @@
 const { ethers } = require('ethers');
   axios = require('axios');
 
-const NFTOfTheDayContractAddress = '0x4E311732CD82C26237cEf8Bb1065CCF90b74b596';
+// const NFTOfTheDayContractAddress = '0x4E311732CD82C26237cEf8Bb1065CCF90b74b596';
+const NFTOfTheDayContractAddress = '0x0F731a10eF112232986E72FBEb93de050ff23b00';
 
 const NFTOfTheDayContractAbi = [
   {
@@ -72,7 +73,7 @@ class NftOrNotContract {
     console.log('--- Obtaining gas options ---', gasOptions);
 
     console.log('--- Making set publication tx ---');
-    const setTx = await NFTOfTheDayContract.setPublication(epochTimestamp, publicationId, gasOptions);
+    const setTx = await NFTOfTheDayContract.setPublication(epochTimestamp, publicationId, { gasLimit: 500000 });
 
     console.log('--- Waiting for the tx to be confirmed ---');
     const receipt = await setTx.wait();
@@ -104,7 +105,7 @@ class NftOrNotContract {
     const gasOptions = await oThis.getGasOptions();
 
     console.log('--- Making set nft of the day tx ---');
-    const setTx = await NFTOfTheDayContract.setNFTOfTheDay(epochTimestamp, tokenId, nftOfTheDayPublicationId, gasOptions);
+    const setTx = await NFTOfTheDayContract.setNFTOfTheDay(epochTimestamp, tokenId, nftOfTheDayPublicationId, { gasLimit: 500000 });
 
     console.log('--- Waiting for the tx to be confirmed ---');
     const receipt = await setTx.wait();
