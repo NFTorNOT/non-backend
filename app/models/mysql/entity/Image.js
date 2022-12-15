7d3998d6f34af67fb96ab8b3a816a0e85e4c7116
vindeep07 <vindeepchaudhari@gmail.com>
2022-12-15 16:19:13 +0530

    Changes in images table

--- app/models/mysql/entity/Image.js
+++ app/models/mysql/entity/Image.js
@@ -4,7 +4,7 @@ const rootPrefix = '../../../..',
   imageConstants = require(rootPrefix + '/lib/globalConstant/entity/image'),
   databaseConstants = require(rootPrefix + '/lib/globalConstant/database');
 
-const dbName = databaseConstants.entityDbName;
+const dbName = databaseConstants.mainDbName;
 
 /**
  * Class for image model.
@@ -32,11 +32,8 @@ class Image extends ModelBase {
    *
    * @param {object} dbRow
    * @param {number} dbRow.id
-   * @param {string} dbRow.url_template
-   * @param {string} dbRow.resolutions
+   * @param {string} dbRow.shortened_url
    * @param {number} dbRow.status
-   * @param {number} dbRow.resize_status
-   * @param {number} dbRow.kind
    * @param {string} dbRow.created_at
    * @param {string} dbRow.updated_at
    *
@@ -48,11 +45,9 @@ class Image extends ModelBase {
 
     const formattedData = {
       id: dbRow.id,
-      urlTemplate: dbRow.url_template,
-      resolutions: JSON.parse(dbRow.resolutions),
+      shortenedUrl: dbRow.shortened_url,
+      url: null, // Add url shortening logic
       status: imageConstants.statuses[dbRow.status],
-      resizeStatus: imageConstants.resizeStatuses[dbRow.resize_status],
-      kind: imageConstants.kinds[dbRow.kind],
       createdAt: dbRow.created_at,
       updatedAt: dbRow.updated_at
     };
@@ -79,57 +74,10 @@ class Image extends ModelBase {
 
     for (let index = 0; index < dbRows.length; index++) {
       const formatDbRow = oThis._formatDbData(dbRows[index]);
-      formatDbRow.resolutions = oThis._formatResolutions(formatDbRow.resolutions, formatDbRow.urlTemplate);
       response[formatDbRow.id] = formatDbRow;
     }
 
     return response;
-  }
-
-  /**
-   * Formats the complete resolution hash.
-   *
-   * @param {object} resolutions
-   * @param {string} urlTemplate
-   *
-   * @returns {object}
-   * @private
-   */
-  _formatResolutions(resolutions, urlTemplate) {
-    const oThis = this;
-
-    const responseResolutionHash = {};
-    for (const resolution in resolutions) {
-      const longResolutionKey = imageConstants.invertedResolutionKeyToShortMap[resolution];
-
-      responseResolutionHash[longResolutionKey] = oThis._formatResolution(resolutions[resolution]);
-
-      const longResolutionSize = longResolutionKey === imageConstants.originalResolution ? '' : longResolutionKey;
-      responseResolutionHash[longResolutionKey].url = shortToLongUrl.getFullUrl(urlTemplate, longResolutionSize);
-    }
-
-    return responseResolutionHash;
-  }
-
-  /**
-   * Format resolutions hash.
-   *
-   * @param {object} resolution
-   *
-   * @returns {object}
-   * @private
-   */
-  _formatResolution(resolution) {
-    const oThis = this;
-
-    const formattedData = {
-      url: resolution.u,
-      size: resolution.s,
-      height: resolution.h,
-      width: resolution.w
-    };
-
-    return oThis.sanitizeFormattedData(formattedData);
   }
 
   /**
@@ -146,108 +94,12 @@ class Image extends ModelBase {
   async insertImage(params) {
     const oThis = this;
 
-    const resolutions = oThis._formatResolutionsToInsert(params.resolutions);
-
     return oThis
       .insert({
-        url_template: params.urlTemplate,
-        resolutions: JSON.stringify(resolutions),
-        kind: imageConstants.invertedKinds[params.kind],
-        status: imageConstants.invertedStatuses[imageConstants.activeStatus],
-        resize_status: imageConstants.invertedResizeStatuses[params.resizeStatus]
+        shortenedUrl: params.urlTemplate,
+        status: imageConstants.invertedStatuses[imageConstants.activeStatus]
       })
       .fire();
-  }
-
-  /**
-   * Update image.
-   *
-   * @param {object} params
-   * @param {string} params.urlTemplate
-   * @param {string} params.resolutions
-   * @param {string} params.resizeStatus
-   * @param {number} params.id
-   *
-   * @returns {object}
-   */
-  async updateImage(params) {
-    const oThis = this;
-
-    console.log('JSON.stringify(params) from updateImage', JSON.stringify(params));
-
-    const resolutions = oThis._formatResolutionsToUpdate(params.resolutions);
-
-    return oThis
-      .update({
-        url_template: params.urlTemplate,
-        resolutions: JSON.stringify(resolutions),
-        resize_status: imageConstants.invertedResizeStatuses[params.resizeStatus]
-      })
-      .where({ id: params.id })
-      .fire();
-  }
-
-  /**
-   * Format resolutions to insert.
-   *
-   * @param {object} resolutions
-   *
-   * @returns {object}
-   * @private
-   */
-  _formatResolutionsToInsert(resolutions) {
-    const oThis = this;
-
-    const responseResolutionHash = {};
-    for (const resolution in resolutions) {
-      const shortResolutionKey = imageConstants.resolutionKeyToShortMap[resolution];
-
-      responseResolutionHash[shortResolutionKey] = oThis._formatResolutionToInsert(resolutions[resolution]);
-    }
-
-    return responseResolutionHash;
-  }
-
-  /**
-   * Format resolutions to update.
-   *
-   * @param {object} resolutions
-   * @private
-   */
-  _formatResolutionsToUpdate(resolutions) {
-    const oThis = this;
-
-    const responseResolutionHash = {};
-    for (const resolution in resolutions) {
-      const shortResolutionKey = imageConstants.resolutionKeyToShortMap[resolution];
-      responseResolutionHash[shortResolutionKey] = responseResolutionHash[shortResolutionKey] || {};
-      Object.assign(
-        responseResolutionHash[shortResolutionKey],
-        oThis._formatResolutionToInsert(resolutions[resolution])
-      );
-    }
-
-    return responseResolutionHash;
-  }
-
-  /**
-   * Format resolution to insert
-   *
-   * @param {object} resolution
-   *
-   * @returns {object}
-   * @private
-   */
-  _formatResolutionToInsert(resolution) {
-    const oThis = this;
-
-    const formattedData = {
-      s: resolution.size,
-      h: resolution.height,
-      w: resolution.width
-    };
-
-    return oThis.sanitizeFormattedData(formattedData);
   }
 }
 
