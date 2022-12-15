87daa20feb72f5e25bfe44fc9f1902e6776cd11a
vindeep07 <vindeepchaudhari@gmail.com>
2022-12-15 15:04:06 +0530

    Changes in images table

--- db/migration/1650872238339_createImagesTable.js
+++ db/migration/1650872238339_createImagesTable.js
@@ -8,10 +8,8 @@ const dbKind = DbKindConstant.sqlDbKind;
 const upQuery =
   'CREATE TABLE `images` ( \n' +
   '`id` bigint unsigned NOT NULL AUTO_INCREMENT, \n' +
-  '`url_template` varchar(500) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL, \n' +
-  '`resolutions` text CHARACTER SET utf8 COLLATE utf8_unicode_ci, \n' +
+  '`shortened_url` varchar(500) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL, \n' +
   '`status` tinyint unsigned NOT NULL DEFAULT 1, \n' +
-  '`kind` tinyint unsigned NOT NULL, \n' +
   '`created_at` int NOT NULL, \n' +
   '`updated_at` int NOT NULL, \n' +
   'PRIMARY KEY (`id`) \n' +
