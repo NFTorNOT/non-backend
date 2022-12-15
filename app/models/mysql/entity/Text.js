6066e3675ddf3f46a139d04f7f8aef8ff0bcb653
dhruvtailor7 <dhruv@plgworks.com>
2022-12-15 11:34:24 +0530

    added text modal and migration

---
+++ app/models/mysql/entity/Text.js
@@ -0,0 +1,96 @@
+const rootPrefix = '../../../..',
+  ModelBase = require(rootPrefix + '/app/models/mysql/Base'),
+  databaseConstants = require(rootPrefix + '/lib/globalConstant/database');
+
+const dbName = databaseConstants.mainDbName;
+
+/**
+ * Class for text model.
+ *
+ * @class Text
+ */
+class Text extends ModelBase {
+  /**
+   * Constructor for text model.
+   *
+   * @augments ModelBase
+   *
+   * @constructor
+   */
+  constructor() {
+    super({ dbName: dbName });
+
+    const oThis = this;
+
+    oThis.tableName = 'texts';
+  }
+
+  /**
+   * Format Db data.
+   *
+   * @param {object} dbRow
+   * @param {number} dbRow.id
+   * @param {string} dbRow.text
+   * @param {string} dbRow.created_at
+   * @param {string} dbRow.updated_at
+   *
+   * @returns {object}
+   * @private
+   */
+  _formatDbData(dbRow) {
+    const oThis = this;
+
+    const formattedData = {
+      id: dbRow.id,
+      text: dbRow.text,
+      createdAt: dbRow.created_at,
+      updatedAt: dbRow.updated_at
+    };
+
+    return oThis.sanitizeFormattedData(formattedData);
+  }
+
+  /**
+   * Insert into texts.
+   *
+   * @param {object} params
+   * @param {string} params.text
+   *
+   * @returns {object}
+   */
+  async insertText(params) {
+    const oThis = this;
+
+    return oThis
+      .insert({
+        text: params.text
+      })
+      .fire();
+  }
+
+  /**
+   * Fetch texts by ids.
+   *
+   * @param {array} id
+   *
+   * @returns {object}
+   */
+  async fetchByIds(id) {
+    const oThis = this;
+
+    const dbRows = await oThis
+      .select('*')
+      .where(['id IN (?)', id])
+      .fire();
+
+    const response = {};
+    for (let index = 0; index < dbRows.length; index++) {
+      const formattedRow = oThis._formatDbData(dbRows[index]);
+      response[formattedRow.id] = formattedRow;
+    }
+
+    return response;
+  }
+}
+
+module.exports = Text;
---
+++ db/migration/1671008953407_createTextsTable.js
@@ -0,0 +1,27 @@
+const rootPrefix = '../..',
+  database = require(rootPrefix + '/lib/globalConstant/database'),
+  DbKindConstant = require(rootPrefix + '/lib/globalConstant/dbKind');
+
+const dbName = database.mainDbName;
+const dbKind = DbKindConstant.sqlDbKind;
+
+const upQuery =
+  'CREATE TABLE `texts` (\n' +
+  '`id` bigint unsigned NOT NULL AUTO_INCREMENT, \n' +
+  '`text` varchar(2048) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL, \n' +
+  '`created_at` timestamp NOT NULL, \n' +
+  '`updated_at` timestamp NOT NULL, \n' +
+  'PRIMARY KEY (`id`) \n' +
+  ') ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;';
+
+const downQuery = 'DROP table if exists texts;';
+
+const migrationName = {
+  dbName: dbName,
+  up: [upQuery],
+  down: [downQuery],
+  dbKind: dbKind,
+  tables: ['texts']
+};
+
+module.exports = migrationName;
